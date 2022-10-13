b08a758540ba6a24933302dcbc97a5f39648f6e4
Ricardo Venieris <rvenieris@gmail.com>
2022-10-13 03:27:51 -0300

    00 - Projeto Vazio

---
+++ SwiftUIChat/SwiftUIChatApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUIChatApp.swift
+//  SwiftUIChat
+//
+//  Created by Ricardo Venieris on 13/10/22.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUIChatApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
