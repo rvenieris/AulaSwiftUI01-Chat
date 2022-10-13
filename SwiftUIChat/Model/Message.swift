ced480ff67ebe2c5376464376cefa2b4aa0f2264
Ricardo Venieris <rvenieris@gmail.com>
2022-10-13 03:57:44 -0300

    Model Criado

---
+++ SwiftUIChat/Model/Message.swift
@@ -0,0 +1,17 @@
+//
+//  Message.swift
+//  InstantMagicChat
+//
+//  Created by Ricardo Venieris on 23/09/20.
+//
+
+import Foundation
+import CloudKitMagicCRUD
+
+struct Message:CKMRecord {
+	var recordName: String?
+	var sender:String
+	var content:String
+	var timestamp:Date = Date()
+	var id:String {self.recordName ?? String(self.hashValue)}
+}
